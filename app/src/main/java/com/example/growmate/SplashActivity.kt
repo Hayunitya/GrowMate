71da28ccb25aeb32ab5747bf6a16a869ce304e39
Hayunitya Edadwi Pratita <146114358+Hayunitya@users.noreply.github.com>
2025-07-21 19:47:19 +0700

    tiap run emulator ga harus login terus

--- app/src/main/java/com/example/growmate/SplashActivity.kt
+++ app/src/main/java/com/example/growmate/SplashActivity.kt
@@ -1,35 +1,37 @@
 package com.example.growmate
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.growmate.databinding.ActivitySplashBinding
+import com.google.firebase.auth.FirebaseAuth
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Inisialisasi ViewBinding
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // ANIMASI: Logo muncul dengan fade-in
         binding.logoImage.alpha = 0f
         binding.logoImage.animate()
             .alpha(1f)
             .setDuration(1000)
             .start()
 
-        // Delay 2 detik lalu pindah ke MainActivity
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, LoginActivity::class.java)
+            val auth = FirebaseAuth.getInstance()
+            val intent = if (auth.currentUser != null) {
+                Intent(this, MainActivity::class.java)  // ✅ User masih login ➔ ke Home
+            } else {
+                Intent(this, LoginActivity::class.java) // ❌ Belum login ➔ ke Login
+            }
             startActivity(intent)
             finish()
         }, 2000)
